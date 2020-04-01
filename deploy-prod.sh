1f970a2473467c3fb5fb9f5534243f59dde1133b
Tenergy Innovation <tenergy.innovation@gmail.com>
2020-04-01 23:04:08 +0700

    update deploy-prod.sh
    update deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,6 @@
 
-PROJECT_ID=gcloud-ota-update
+# PROJECT_ID=gcloud-ota-update
+PROJECT_ID=esp8266tenergyota
 gcloud config set project $PROJECT_ID
 gcloud functions deploy insertFirmwaresOnBigquery --runtime nodejs8 --trigger-resource $PROJECT_ID-firmwares --trigger-event google.storage.object.finalize
 gcloud functions deploy getDownloadUrl --runtime nodejs8 --trigger-http
